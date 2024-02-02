aa9c717d2da615f043e68c650db059b3610a8eaa
thaituankha <136249921+thaituankha@users.noreply.github.com>
2024-02-02 09:56:25 +0700

    Add files via upload

---
+++ RSI_Streamlit_Adj.py
@@ -0,0 +1,41 @@
+#import thư viện
+import streamlit as st
+from vnstock import *
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+import warnings
+warnings.filterwarnings("ignore")
+
+data_final =pd.read_excel('caculate_OB_OS_RSI.xlsx')
+data_final['OB-OS'] = data_final['RSI_OB'] - data_final['RSI_OS']
+
+st.dataframe(data_final)
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['RSI_OB'], mode='lines', name='quá mua'))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['RSI_OS'], mode='lines', name='quá bán'))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['VNINDEX'], mode='lines', name='VNINDEX', yaxis='y2'))
+fig.update_layout(
+    title='RSI và VNINDEX',
+    xaxis=dict(title='Thời gian'),
+    yaxis=dict(title='%'),
+    yaxis2=dict(title='VNINDEX', overlaying='y', side='right'),
+    legend=dict(x=0.7, y=1)
+)
+st.plotly_chart(fig)
+
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['OB-OS'], mode='lines', name='quá mua trừ quá bán'))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['VNINDEX'], mode='lines', name='VNINDEX', yaxis='y2'))
+fig.update_layout(
+    title='OB - OS và VNINDEX',
+    xaxis=dict(title='Thời gian'),
+    yaxis=dict(title='%'),
+    yaxis2=dict(title='VNINDEX', overlaying='y', side='right'),
+    legend=dict(x=0.7, y=1)
+)
+st.plotly_chart(fig)
