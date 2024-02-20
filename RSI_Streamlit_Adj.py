8da094b697eb733514436aaab946da2bd75166cf
thaituankha <136249921+thaituankha@users.noreply.github.com>
2024-02-20 11:13:19 +0700

    Add files via upload

--- RSI_Streamlit_Adj.py
+++ RSI_Streamlit_Adj.py
@@ -17,9 +17,9 @@ data_final['OS'] = data_final['RSI_OS_number'] / (data_final['RSI_OB_number']+da
 st.dataframe(data_final)
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['RSI_OB'], mode='lines', name='quá mua'))
-fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['RSI_OS'], mode='lines', name='quá bán'))
-fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['VNINDEX'], mode='lines', name='VNINDEX', yaxis='y2'))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['RSI_OB'], mode='lines', name='quá mua', line=dict(color='green')))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['RSI_OS'], mode='lines', name='quá bán', line=dict(color='red')))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['VNINDEX'], mode='lines', name='VNINDEX', yaxis='y2', line=dict(color='blue')))
 fig.update_layout(
     title='RSI và VNINDEX',
     xaxis=dict(title='Thời gian'),
@@ -31,9 +31,8 @@ st.plotly_chart(fig)
 
 
 fig = go.Figure()
-fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['OB'], mode='lines', name='quá mua'))
-fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['OS'], mode='lines', name='quá bán'))
-#fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['VNINDEX'], mode='lines', name='VNINDEX', yaxis='y2'))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['OB'], mode='lines', name='quá mua', line=dict(color='green')))
+fig.add_trace(go.Scatter(x=data_final['date'], y=data_final['OS'], mode='lines', name='quá bán', line=dict(color='red')))
 fig.update_layout(
     title='Tỷ lệ quá mua/quá bán trên tổng quá mua, quá bán và VNINDEX',
     xaxis=dict(title='Thời gian'),
@@ -42,3 +41,21 @@ fig.update_layout(
     legend=dict(x=0.7, y=1)
 )
 st.plotly_chart(fig)
+
+
+#biểu đồ SMA200
+data_SMA_200 = pd.read_csv('data_SMA200.csv')
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=data_SMA_200['date'], y=data_SMA_200['% CP có giá trên SMA200'], mode='lines', name='% CP có giá trên SMA200', line=dict(color='green')))
+fig.add_trace(go.Scatter(x=data_SMA_200['date'], y=data_SMA_200['% CP có giá dưới SMA200'], mode='lines', name='% CP có giá dưới SMA200', line=dict(color='red')))
+fig.add_trace(go.Scatter(x=data_SMA_200['date'], y=data_SMA_200['index'], mode='lines', name='Vnindex', yaxis='y2', line=dict(color='blue')))
+fig.update_layout(
+    title='SMA200',
+    xaxis=dict(title='Thời gian'),
+    yaxis=dict(title='%'),
+    yaxis2=dict(title='VNINDEX', overlaying='y', side='right'),
+    legend=dict(x=0.7, y=1)
+)
+st.plotly_chart(fig)
+
